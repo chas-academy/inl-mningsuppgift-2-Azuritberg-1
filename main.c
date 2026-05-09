a722ab0fce7855eb95e8743654b7b3a147e235a1
bojanalukacgmailcom <bojana.lukac@gmail.com>
2026-05-09 17:49:18 +0200

    Try code

--- main.c
+++ main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 
-
 // This is where you add the two functions
 
-int main() {
+float fahrenheit_to_celsius(float temp_f)
+{
+    float temp_c = (temp_f - 32) * 5.0 / 9.0;
+    return temp_c;
+}
+
+float celsius_to_fahrenheit(float temp_c)
+{
+    float temp_f = (temp_c * 9.0 / 5.0) + 32;
+    return temp_f;
+}
+
+int main()
+{
     float temp_f = 100.0;
     float temp_c = 37.0;
 
-    printf("%.1f\n", fahrenheit_to_celsius(temp_f));  // Expected output: 37.8
-    printf("%.1f\n", celsius_to_fahrenheit(temp_c));  // Expected output: 98.6
+    printf("%.1f\n", fahrenheit_to_celsius(temp_f)); // Expected output: 37.8
+    printf("%.1f\n", celsius_to_fahrenheit(temp_c)); // Expected output: 98.6
 
     return 0;
 }
